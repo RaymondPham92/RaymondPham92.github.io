1e7a61506480c3226e6d975f43fdd1484f476e85
Raymond Pham <raymond.pham43@gmail.com>
2024-01-31 13:58:37 +1100

    progress

--- weather_website/src/App.js
+++ weather_website/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import axios from 'axios'
+
 
 function App() {
+  const API_key = 'dbafa9aab1ad78fe793475dee0990fd1'
+  const city_name ='Sydney'
+
+  // get latitude and longitude from city name
+  const geocoding_url = `http://api.openweathermap.org/geo/1.0/direct?q=${city_name}&limit=5&appid=${API_key}`
+
+
+  const weather_url = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${API_key}`
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. hi
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <div className='top'>
+          <div className='location'>
+            <p>Sydney</p>
+          </div>
+          <div className='temp'>
+            <h1>10°C</h1>
+          </div>
+          <div className='desc'>
+            <p>Clouds</p>
+          </div>
+        </div>
+
+        <div className='bot'>
+          <div className='feels_like'>
+            <p>15°C</p>
+          </div>
+          <div className='humidity'>
+            <p>20%</p>
+          </div>
+          <div className='wind'>
+            <p>10 km/h</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
