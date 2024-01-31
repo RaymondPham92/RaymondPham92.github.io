0c50a292f5af101575d8c491e0d6dd8840fd5030
Raymond Pham <raymond.pham43@gmail.com>
2024-01-31 18:25:56 +1100

    progress

--- weather_website/src/App.js
+++ weather_website/src/App.js
@@ -4,17 +4,54 @@ import axios from 'axios'
 
 function App() {
   const API_key = 'dbafa9aab1ad78fe793475dee0990fd1'
-  const city_name ='Sydney'
+
+  const [data, setData] = useState({})
+  const [cityName, setCityName] = useState("")
+  const [weatherData, setWeatherData] = useState({})
+
+  var lat = 0
+  var lon = 0
 
   // get latitude and longitude from city name
-  const geocoding_url = `http://api.openweathermap.org/geo/1.0/direct?q=${city_name}&limit=5&appid=${API_key}`
+  const geocoding_url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${API_key}`
 
-
-  const weather_url = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${API_key}`
-
+  const searchLocation = (event) =>{
+    if (cityName === '') {
+      return
+    }
+    axios.get(geocoding_url).then((response) => {
+      setData(response.data[0])
+      lat = data.lat
+      lon = data.lon
+      console.log(data)
+      const weather_url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}`
+      axios.get(weather_url).then((response) => {
+        setWeatherData(response.data)
+        console.log(weatherData)
+        setCityName('')
+      })
+      
+    })
+  }
 
   return (
     <div className="App">
+      <div className='search'>
+        <input
+          id='search' 
+          value={cityName}
+          onChange={event => setCityName(event.target.value)}
+          placeholder='Enter city name'
+          type='text'
+        >
+        </input>
+        <button 
+          onClick={searchLocation}
+        >
+          Search
+        </button>
+      </div>
+
       <div className='container'>
         <div className='top'>
           <div className='location'>
@@ -30,13 +67,16 @@ function App() {
 
         <div className='bot'>
           <div className='feels_like'>
-            <p>15°C</p>
+            <p className='bold'>15°C</p>
+            <p>Feels Like</p>
           </div>
           <div className='humidity'>
-            <p>20%</p>
+            <p className='bold'>20%</p>
+            <p>Humidity</p>
           </div>
           <div className='wind'>
-            <p>10 km/h</p>
+            <p className='bold'>10 km/h</p>
+            <p>Wind Speed</p>
           </div>
         </div>
       </div>
